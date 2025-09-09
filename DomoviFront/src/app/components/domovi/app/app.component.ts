2fbbbbd4f2ddf814a4875dd8e55456e5accb9767
Sekula2023 <sekulic.sr44.2022@uns.ac.rs>
2025-09-09 11:00:54 +0200

    quick fix

--- DomoviFront/src/app/components/domovi/app/app.component.ts
+++ DomoviFront/src/app/components/domovi/app/app.component.ts
@@ -1,26 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-import { FaviconService } from '../../../services/favicon,service';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  standalone: true,
+  imports: [RouterOutlet],
+  template: `
+    <router-outlet></router-outlet>
+  `
 })
-export class AppComponent implements OnInit {
-
-  constructor(private router: Router, private faviconService: FaviconService) {}
-
-  ngOnInit() {
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        // Promeni favicon u zavisnosti od rute
- if (event.url.includes('home')) {
-          this.faviconService.setFavicon('assets/dormitory.png');
-        } else {
-          this.faviconService.setFavicon('assets/favicon.ico');
-        }
-      }
-    });
-  }
-}
+export class AppComponent {}
